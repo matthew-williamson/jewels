e1a1e09845ad4a5c49666cdf15922c8e4c3a70c5
Tyler Herrick <therrick47@ymail.com>
2023-12-04 07:23:32 -0700

    adding changes built off shopify demo

---
+++ app/constants.js
@@ -0,0 +1,173 @@
+export const alphabet = {
+  A: {
+    id: "A",
+    name: "A",
+    src: "./images/Pendants/alpha-a2.png",
+    price: 2
+  },
+  B: {
+    id: "B",
+    name: "B",
+    src: "./images/Pendants/alpha-B2.png",
+    price: 2
+  },
+  C: {
+    id: "C",
+    name: "C",
+    src: "./images/Pendants/alpha-C.png",
+    price: 2
+  },
+  D: {
+    id: "D",
+    name: "D",
+    src: "./images/Pendants/alpha-D.png",
+    price: 2
+  },
+  E: {
+    id: "E",
+    name: "E",
+    src: "./images/Pendants/alpha-E.png",
+    price: 2
+  },
+  F: {
+    id: "F",
+    name: "F",
+    src: "./images/Pendants/alpha-F.png",
+    price: 2
+  },
+  G: {
+    id: "G",
+    name: "G",
+    src: "./images/Pendants/alpha-G.png",
+    price: 2
+  },
+  H: {
+    id: "H",
+    name: "H",
+    src: "./images/Pendants/alpha-H.png",
+    price: 2
+  },
+  I: {
+    id: "I",
+    name: "I",
+    src: "./images/Pendants/alpha-I.png",
+    price: 2
+  },
+  J: {
+    id: "J",
+    name: "J",
+    src: "./images/Pendants/alpha-J.png",
+    price: 2
+  },
+  K: {
+    id: "K",
+    name: "K",
+    src: "./images/Pendants/alpha-K.png",
+    price: 2
+  },
+  L: {
+    id: "L",
+    name: "L",
+    src: "./images/Pendants/alpha-L.png",
+    price: 2
+  },
+  M: {
+    id: "M",
+    name: "M",
+    src: "./images/Pendants/alpha-M.png",
+    price: 2
+  },
+  N: {
+    id: "N",
+    name: "N",
+    src: "./images/Pendants/alpha-N.png",
+    price: 2
+  },
+  O: {
+    id: "O",
+    name: "O",
+    src: "./images/Pendants/alpha-O.png",
+    price: 2
+  },
+  P: {
+    id: "P",
+    name: "P",
+    src: "./images/Pendants/alpha-P.png",
+    price: 2
+  },
+  Q: {
+    id: "Q",
+    name: "Q",
+    src: "./images/Pendants/alpha-Q.png",
+    price: 2
+  },
+  R: {
+    id: "R",
+    name: "R",
+    src: "./images/Pendants/alpha-R.png",
+    price: 2
+  },
+  S: {
+    id: "S",
+    name: "S",
+    src: "./images/Pendants/alpha-S.png",
+    price: 2
+  },
+  T: {
+    id: "T",
+    name: "T",
+    src: "./images/Pendants/alpha-T.png",
+    price: 2
+  },
+  U: {
+    id: "U",
+    name: "U",
+    src: "./images/Pendants/alpha-U.png",
+    price: 2
+  },
+  V: {
+    id: "V",
+    name: "V",
+    src: "./images/Pendants/alpha-V.png",
+    price: 2
+  },
+  W: {
+    id: "W",
+    name: "W",
+    src: "./images/Pendants/alpha-W.png",
+    price: 2
+  },
+  X: {
+    id: "X",
+    name: "X",
+    src: "./images/Pendants/alpha-X.png",
+    price: 2
+  },
+  Y: {
+    id: "Y",
+    name: "Y",
+    src: "./images/Pendants/alpha-Y.png",
+    price: 2
+  },
+  Z: {
+    id: "Z",
+    name: "Z",
+    src: "./images/Pendants/alpha-Z.png",
+    price: 2
+  },
+};
+
+export const chains = {
+  simpleGold: {
+    price: 99,
+    id: "simpleGold",
+    name: "Simple Gold",
+    src: "../images/Chains/chaingold.png",
+  },
+  elaborateGold: {
+    price: 110,
+    id: "elaborateGold",
+    name: "Elaborate Gold",
+    src: "../images/Chains/chaingoldelaborate2.png",
+  },
+};
---
+++ app/routes/app._index.jsx
@@ -0,0 +1,327 @@
+import { useCallback, useEffect, useState } from "react";
+import { json } from "@remix-run/node";
+import { useActionData, useNavigation, useSubmit } from "@remix-run/react";
+import {
+  Page,
+  Layout,
+  Text,
+  Card,
+  Button,
+  BlockStack,
+  Box,
+  List,
+  Link,
+  InlineStack,
+} from "@shopify/polaris";
+import {
+  Checkbox,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Image, Layer, Stage } from "react-konva";
+import { authenticate } from "../shopify.server";
+import { alphabet, chains } from "../constants";
+import PendantImage from "../PendantImage";
+import PendantSelectionPanel from "../PendantSelectionPanel";
+import useImage from "use-image";
+
+const URLImage = ({ src, height, width, x = 0, y = 0, rotation = 0 }) => {
+  const [image] = useImage(src);
+  return <Image image={image} height={height} width={width} x={x} y={y} rotation={rotation} />;
+};
+
+export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+
+  return null;
+};
+export const action = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+  const color = ["Red", "Orange", "Yellow", "Green"][
+    Math.floor(Math.random() * 4)
+  ];
+  const response = await admin.graphql(
+    `#graphql
+      mutation populateProduct($input: ProductInput!) {
+        productCreate(input: $input) {
+          product {
+            id
+            title
+            handle
+            status
+            variants(first: 10) {
+              edges {
+                node {
+                  id
+                  price
+                  barcode
+                  createdAt
+                }
+              }
+            }
+          }
+        }
+      }`,
+    // {
+    //   variables: {
+    //     input: {
+    //       title: `${color} Snowboard`,
+    //       variants: [{ price: Math.random() * 100 }],
+    //     },
+    //   },
+    // }
+  );
+  const responseJson = await response.json();
+
+  return json({
+    product: responseJson.data.productCreate.product,
+  });
+};
+
+export default function Index() {
+  const nav = useNavigation();
+  const actionData = useActionData();
+  const submit = useSubmit();
+  console.log("pretest");
+  const isLoading =
+    ["loading", "submitting"].includes(nav.state) && nav.formMethod === "POST";
+  console.log(actionData);
+  console.log(actionData?.product);
+  const productId = actionData?.product?.id.replace(
+    "gid://shopify/Product/",
+    ""
+  );
+  console.log(productId);
+  console.log("posttest");
+
+  const [price, setPrice] = useState(99);
+  const [isAddingPersonalNote, setIsAddingPersonalNote] = useState(false);
+  const [personalNote, setPersonalNote] = useState("");
+  const [centerpiece, setCenterpiece] = useState(alphabet.A);
+  const [leftpiece, setLeftpiece] = useState(alphabet.A);
+  const [rightpiece, setRightpiece] = useState(alphabet.A);
+  const [chain, setChain] = useState(chains.simpleGold);
+  const [letter, setLetter] = useState(alphabet.A);
+
+  //useEffect only runs setPrice when [] list updates any values
+  useEffect(() => {
+    setPrice(centerpiece.price + leftpiece.price + rightpiece.price + (isAddingPersonalNote === true ? 2 : 0));
+  }, [centerpiece.price, leftpiece.price, rightpiece.price, isAddingPersonalNote]);
+
+  //   setPrice(chain.price + centerpiece.price + leftpiece.price + rightpiece.price + (isAddingPersonalNote === true ? 2 : 0));
+  // }, [chain.price, centerpiece.price, leftpiece.price, rightpiece.price, isAddingPersonalNote]);
+
+  //useCallback only calculates function (input,output dictionary values)
+  //when a dependency value changes
+  const handleOnCenterpieceChange = useCallback((newCenter) => {
+    setCenterpiece(newCenter);
+  });
+
+  const handleOnLeftpieceChange = useCallback((newLeft) => {
+    setLeftpiece(newLeft);
+  });
+
+  const handleOnRightpieceChange = useCallback((newRight) => {
+    setRightpiece(newRight);
+  });
+  const handleOnChainChange = useCallback((newChain) => {
+    // setChain(newChain);
+  }, []);
+
+  const handleOnLetterChange = useCallback((newLetter) => {
+    setLetter(newLetter);
+  });
+
+  const onAddToCartClick = useCallback((e) => {
+    console.log("TODO");
+  }, []);
+
+  const handleAddPersonalNoteChange = useCallback((e) => {
+    setIsAddingPersonalNote(e.target.checked);
+  });
+
+  const handlePersonalNoteChange = useCallback((e) => {
+    setPersonalNote(e.target.value);
+  }, []);
+
+
+  useEffect(() => {
+    if (productId) {
+      shopify.toast.show("Product created");
+    }
+  }, [productId]);
+  const generateProduct = () => submit({}, { replace: true, method: "POST" });
+
+  return (
+    <Page>
+      <ui-title-bar title="Remix app template">
+
+      </ui-title-bar>
+      <BlockStack gap="500">
+
+        <Layout>
+          <Paper
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: { xs: 1.75, sm: 4 },
+              flexWrap: "wrap",
+              py: 1,
+              px: 0.5,
+              borderRadius: "25px",
+            }}
+          >
+            <BlockStack spacing={2} direction="row" sx={{ maxWidth: 1200 }}>
+              <Layout>
+                <Layout.Section>
+                  <Paper sx={{ py: 2, px: 2 }}>
+                    <Stack spacing={2}>
+                      <Box sx={{ borderRadius: 2 }} width={500} height={600}>
+                        <Stage width={500} height={600}>
+                          <Layer>
+                            {/* chain */}
+                            <URLImage
+                              src={chains[chain.id].src}
+                              width={500}
+                              height={600}
+                            />
+                          </Layer>
+                          {/*centerpiece*/}
+                          <PendantImage
+                            newSrc={alphabet[centerpiece.id].src}
+                            newWidth={300}
+                            newHeight={300}
+                            newX={100}
+                            newY={255}
+                            newRotation={0}
+                          />
+                          {/*leftpiece*/}
+                          <PendantImage
+                            newSrc={alphabet[leftpiece.id].src}
+                            newWidth={300}
+                            newHeight={300}
+                            newX={180}
+                            newY={170}
+                            newRotation={45}
+                          />
+                          {/*rightpiece*/}
+                          <PendantImage
+                            newSrc={alphabet[rightpiece.id].src}
+                            newWidth={300}
+                            newHeight={300}
+                            newX={100}
+                            newY={380}
+                            newRotation={-45}
+                          />
+                        </Stage>
+                      </Box>
+                      <Stack direction="row" sx={{ justifyContent: "space-between" }}>
+                        <Typography>Price: ${price}</Typography>
+                        <Button onClick={onAddToCartClick} variant="outlined">
+                          Add to Cart
+                        </Button>
+                      </Stack>
+                      <Stack direction="row" sx={{ alignItems: "center" }}>
+                        <Checkbox onChange={handleAddPersonalNoteChange} />
+                        <Typography>Add a personal note (+$2)</Typography>
+                      </Stack>
+                      <TextField
+                        disabled={!isAddingPersonalNote}
+                        onChange={handlePersonalNoteChange}
+                        value={personalNote}
+                      />
+                    </Stack>
+                  </Paper>
+                </Layout.Section>
+                <Layout.Section variant="oneThird">
+                  <BlockStack spacing={2}>
+                    <Paper sx={{ py: 2, px: 2 }}>
+                      <BlockStack spacing={1}>
+                        <Typography>Step 1: Choose your chain</Typography>
+                        <BlockStack spacing={1} direction="row">
+                          <Box
+                            sx={{
+                              width: 100,
+                              height: 100,
+                              backgroundColor: "gray",
+                              borderRadius: 2,
+                              overflow: "hidden",
+                              border:
+                                chain.id === "simpleGold"
+                                  ? "1px solid teal"
+                                  : "1px solid lightgray",
+                              cursor: "pointer",
+                              ":hover": {
+                                backgroundColor: "lightgray",
+                                boxShadow: "0 0 0 1px lightgray",
+                              },
+                            }}
+                          // onClick={() => handleOnChainChange(chains.simpleGold)}
+                          >
+                            <img
+                              src="./images/Chains/chaingold.png"
+                              alt="simple gold chain"
+                              objectFit="cover"
+                              width="100%"
+                              height="100%"
+                            />
+                          </Box>
+                          <Box
+                            sx={{
+                              width: 100,
+                              height: 100,
+                              backgroundColor: "gray",
+                              border:
+                                chain.id === "elaborateGold"
+                                  ? "1px solid teal"
+                                  : "1px solid lightgray",
+                              borderRadius: 2,
+                              overflow: "hidden",
+                              cursor: "pointer",
+                              ":hover": {
+                                backgroundColor: "lightgray",
+                                boxShadow: "0 0 0 1px lightgray",
+                              },
+                            }}
+                          // onClick={() => handleOnChainChange(chains.elaborateGold)}
+                          >
+                            <img
+                              src="./images/Chains/chaingoldelaborate2.png"
+                              alt="simple gold chain"
+                              objectFit="cover"
+                              width="100%"
+                              height="100%"
+                            />
+                          </Box>
+                          <Box
+                            sx={{
+                              width: 100,
+                              height: 100,
+                              backgroundColor: "gray",
+                              borderRadius: 2,
+                            }}
+                          />
+                          <Box
+                            sx={{
+                              width: 100,
+                              height: 100,
+                              backgroundColor: "gray",
+                              borderRadius: 2,
+                            }}
+                          />
+                        </BlockStack>
+                      </BlockStack>
+                    </Paper>
+                  </BlockStack>
+                </Layout.Section>
+              </Layout>
+            </BlockStack>
+          </Paper>
+        </Layout>
+      </BlockStack>
+    </Page>
+  );
+}
